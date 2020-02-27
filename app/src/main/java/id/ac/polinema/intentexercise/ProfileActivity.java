88288e87ec2ba600f25a5b11f8950a938472a915
yasatria <satriamarfel@gmail.com>
2020-02-28 04:46:00 +0700

    membuat profile activity dan handle register

--- app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
@@ -2,13 +2,61 @@ package id.ac.polinema.intentexercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import id.ac.polinema.intentexercise.model.UserModel;
 
 public class ProfileActivity extends AppCompatActivity {
+    private TextView fullName;
+    private TextView email;
+    private TextView homepage;
+    private TextView about;
+    private ImageView imageProfile;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        fullName = findViewById(R.id.label_fullname);
+        email = findViewById(R.id.label_email);
+        homepage = findViewById(R.id.label_homepage);
+        about = findViewById(R.id.label_about);
+        imageProfile = findViewById(R.id.image_profile);
+
+        Bundle extras = getIntent().getExtras();
+
+
+        if (extras != null) {
+            UserModel userModel = extras.getParcelable(RegisterActivity.USER_KEY);
+            fullName.setText(userModel.getFullName());
+            email.setText(userModel.getEmail());
+            homepage.setText(userModel.getHomepage());
+            about.setText(userModel.getAbout());
+            Bitmap bmp = BitmapFactory.decodeByteArray(getIntent().getByteArrayExtra("profileImage"), 0, getIntent().getByteArrayExtra("profileImage").length);
+            imageProfile.setImageBitmap(bmp);
+
+        }
+    }
+
+    public void handleHomepage(View view) {
+        String url = homepage.getText().toString();
+
+        if (!url.startsWith("https://") && !url.startsWith("http://")){
+            url = "http://" + url;
+        }
+        Intent openUrlIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        if (openUrlIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(openUrlIntent);
+        }
+
     }
 }
